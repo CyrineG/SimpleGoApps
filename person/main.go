53632199cd80ae44ef65848f490918f9a1ca6903
CyrineG <syrynegamoudi@gmail.com>
2022-07-12 18:28:44 +0100

    maps

--- person/main.go
+++ person/main.go
@@ -15,19 +15,15 @@ func main(){
 	sami := person{
 		firstname: "Sami", 
 		lastname: "BouAnber",
-		contactInfo: contactInfo{
-			"person@email.com",
-			 123},
+		contactInfo: contactInfo{"person@email.com", 123},
 	}
 
 	(&sami).updateName("ahmed")
 	sami.print()
 }
-
 func (p person) print(){
 	fmt.Println(p)
 }
-
 func (pointerToPerson *person) updateName(newFName string){
 	*&pointerToPerson.firstname = newFName
 }
