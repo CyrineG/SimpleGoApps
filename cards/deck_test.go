d526b89be006bb8e4fe4e82cba45591a3caa5aaa
CyrineG <syrynegamoudi@gmail.com>
2022-07-12 14:08:40 +0100

    test IO

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,10 +1,41 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T){
 	d:= newDeck()
-	if len(d) != 20 {
-		t.Errorf("expected deck length of 20, but got %d", len(d))
+	if len(d) != 16 {
+		t.Errorf("expected deck length of 16, but got %d", len(d))
 	}
+
+	if d[0] != "ace of spades" {
+		t.Errorf("expected d[0] equals 'ace of spades', but got %s", d[0])
+	}
+
+	if d[len(d)-1] != "four of clubs" {
+		t.Errorf("expected d[0] equals 'four of clubs', but got %s", d[len(d)-1])
+	}
+}
+
+
+func TestSaveToFileAndDeckFromFile(t *testing.T){
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	if _, err := os.Stat("_decktesting"); errors.Is(err, os.ErrNotExist) {
+		t.Errorf("expected 'decktesting' to be created, but file doesn't exist")
+	}
+
+	dfile := newDeckFromFile("_decktesting")
+	if len(dfile) != 16 {
+		t.Errorf("expected deck length of 16, but got %d", len(d))
+	}
+
+	os.Remove("_decktesting")
 }
