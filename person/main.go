107f652b9cce0ed69975f71b8a13f6de2663c2b2
CyrineG <syrynegamoudi@gmail.com>
2022-07-12 16:19:55 +0100

    Use pointers

---
+++ person/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email string
+	zip int
+}
+type person struct {
+	firstname string
+	lastname string
+	contactInfo
+}
+func main(){
+	sami := person{
+		firstname: "Sami", 
+		lastname: "BouAnber",
+		contactInfo: contactInfo{
+			"person@email.com",
+			 123},
+	}
+
+	(&sami).updateName("ahmed")
+	sami.print()
+}
+
+func (p person) print(){
+	fmt.Println(p)
+}
+
+func (pointerToPerson *person) updateName(newFName string){
+	*&pointerToPerson.firstname = newFName
+}
